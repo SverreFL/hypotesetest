d4fcd1f2c54fe513be70130626b0306aa8f7e214
sverre <sla097@uib.no>
2021-04-17 15:31:17 +0200

    first commit

---
+++ utilities/helper_functions.py
@@ -0,0 +1,75 @@
+'''
+Litt usikker på om jeg må importere ting til modul når all koden uansett blir kjørt fra annet script ..
+dette handler om namespace.. hvor er det funksjon søker over... der den blir definert eller den blir kjørt. hmhm.
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+import panel as pn; pn.extension()
+import seaborn as sns
+from scipy import stats
+from scipy.integrate import quad
+# utilities
+def get_critical_values(test_dist_hyp, α, μ_true, μ_hyp):
+    t_lower = test_dist_hyp.ppf(α/2)
+    t_upper = test_dist_hyp.ppf(1-α/2)
+    return t_lower, t_upper
+
+def power(dist, t_lower, t_upper):
+        return 1-quad(dist, t_lower, t_upper)[0]
+    
+def make_power_grid(μ_hyp, σ_true, N,  t_lower, t_upper):
+    μ_grid = np.linspace(-2,2, 200)
+    
+    power_grid = np.empty(len(μ_grid))
+    for i in range(len(μ_grid)):
+        dist = stats.norm(loc=μ_grid[i]-μ_hyp, scale=σ_true/np.sqrt(N)).pdf
+        power_grid[i] = power(dist, t_lower, t_upper)
+    return μ_grid, power_grid
+
+def make_power_val(μ_hyp, μ_true, σ_true, N,  t_lower, t_upper):
+    dist = stats.norm(loc=μ_true-μ_hyp, scale=σ_true/np.sqrt(N)).pdf
+    power_val = power(dist, t_lower, t_upper)
+    return power_val
+
+# plot functions
+def make_plot0(test_dist_true, test_dist_hyp, t_lower, t_upper, ax):
+    
+    grid = np.linspace(-2,2, num=200)
+    ax.plot(grid, test_dist_hyp.pdf(grid), linewidth=2, label='nullhypotese')
+    ax.plot(grid, test_dist_true.pdf(grid), linewidth=2, label=r'sann $\mu$')
+    ax.hlines(0, t_lower, t_upper, color='black')
+    ax.vlines(t_lower, -0.05, 0.05, color='black')
+    ax.vlines(t_upper, -0.05, 0.05, color='black')
+    ax.fill_between(np.linspace(t_lower, t_upper),0, 
+                    test_dist_true.pdf(np.linspace(t_lower, t_upper)),
+                    color='tab:orange', alpha=.5)
+    
+    ax.set(xlim=(-2.05, 2.05), ylim=(-0.05,2.5), xlabel='Verdi til testobservator',
+           xticks=np.arange(-2,2+1))
+    ax.legend(title=r'Fordeling der $\mu$ tilsvarer',
+              framealpha=0)
+    ax.title.set_text('Fordeling til testobservatorer')                
+    return ax
+    
+def make_plot1(μ_hyp, μ_true,σ_true, N,  t_lower, t_upper,α, ax):
+    μ_grid, power_grid = make_power_grid(μ_hyp, σ_true, N,  t_lower, t_upper)
+    power_val = make_power_val(μ_hyp, μ_true, σ_true, N,  t_lower, t_upper)
+    
+    ax.plot(μ_grid, power_grid, color='black')
+    ax.scatter(μ_true, power_val, color='tab:orange', s=100)
+    ax.set(xlim=(-2.05, 2.05), ylim = (-.05, 1.05), xlabel=r'Sann $\mu$', ylabel='Styrke',
+          xticks=np.arange(-2,2+1))
+    ax.title.set_text(r'Styrke, 1-P(type-II feil), som funksjon av sann $\mu$')
+    return ax
+    
+def make_plot(μ_true, σ_true, μ_hyp, N, α):
+    dist_true = stats.norm(loc=μ_true, scale=σ_true)
+    test_dist_hyp = stats.norm(loc=0, scale=σ_true/np.sqrt(N))
+    test_dist_true = stats.norm(loc=μ_true-μ_hyp, scale=σ_true/np.sqrt(N))
+    t_lower, t_upper = get_critical_values(test_dist_hyp, α, μ_true, μ_hyp)
+    
+    fig, axes = plt.subplots(1,2, figsize=(14,6))
+    axes[0] = make_plot0(test_dist_true, test_dist_hyp, t_lower, t_upper, axes[0])
+    axes[1] = make_plot1(μ_hyp, μ_true,σ_true, N, t_lower, t_upper, α, axes[1])
+    plt.close(fig)
+    return fig
